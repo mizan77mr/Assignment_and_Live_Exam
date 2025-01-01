058e197fefa4b0ce721b97f2b71a06d5424042b6
Mizanur Rahman <mizanshaheb644@gmail.com>
2025-01-01 23:53:12 +0600

    updated

--- bin/live_test_1.dart
+++ bin/live_test_1.dart
@@ -6,11 +6,34 @@ triangle();
 
 }
 
-//value assign
+
 void triangle(){
 
-  double base=40.0;
+double base=40.0;
+
+  /* print('Enter your triangle base value :');
+ double? base=double.tryParse(stdin.readLineSync()!);*/
+
 double height=20.0;
+
+/* print('Enter your triangle height :');
+ double? height=double.tryParse(stdin.readLineSync()!);*/
+
+
+
+    //if user validate input
+/*if(base!= null && height != null && base>0 && height>0){
+
+    // calculate area
+   double area=0.5*base*height;
+   print('The Triangle Area is : $area');
+}
+ else{
+
+   print(' The Triangle  Area is Invalid');
+
+ }*/
+
 
 double area=0.5*base*height;
 print('The Triangle Area is : $area');
