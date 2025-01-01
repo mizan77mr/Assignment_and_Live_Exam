6fc30fbb3762c83279982b8e1fb49abb8c286d66
Mizanur Rahman <mizanshaheb644@gmail.com>
2025-01-01 23:19:29 +0600

    update

---
+++ bin/live_test_1.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main(){
+
+triangle();
+
+}
+
+//value assign
+void triangle(){
+
+  double base=40.0;
+double height=20.0;
+
+double area=0.5*base*height;
+print('The Triangle Area is : $area');
+
+
+
+}
