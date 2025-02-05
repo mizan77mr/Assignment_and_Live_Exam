33b4fedcbc1a88d8ba63fdc52a01935de7325504
Mizanur Rahman <mizanshaheb644@gmail.com>
2025-02-05 23:16:01 +0600

    complete second live test

---
+++ lib/homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Add Employee',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 30),
+            child: Form(
+                child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Name'),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Age'),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  decoration: InputDecoration(hintText: 'Salary'),
+                ),
+              ],
+            )),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text('Add Employee'))
+        ],
+      ),
+    );
+  }
+}
